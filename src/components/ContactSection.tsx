07b2907339ff33dfdb2e859ce330478e29e7b26a
DOLEV ATIK <DOLEV5454@GMAIL.COM>
2026-01-13 13:48:09 +0200

    Update WhatsApp link text in ContactSection

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -12,7 +12,8 @@ const ContactSection = () => {
       icon: MessageCircle,
       label: "וואטסאפ",
       value: "שלח הודעה",
-      href: "https://wa.me/972546142426?text=%D7%94%D7%99%D7%99%20%D7%90%D7%95%D7%A8%2C%20%D7%90%D7%A9%D7%9E%D7%97%20%D7%9C%D7%A9%D7%9E%D7%95%D7%A2%20%D7%A4%D7%A8%D7%98%D7%99%D7%9D%20%D7%A0%D7%95%D7%A1%D7%A4%D7%99%D7%9D%20%D7%A2%D7%9C%20%D7%94%D7%A9%D7%99%D7%A8%D7%95%D7%AA%D7%99%D7%9D%20%D7%A9%D7%9C%D7%9A"
+      href: https://wa.me/972546142426?text=%D7%94%D7%99%D7%99%20%D7%91%D7%90%D7%AA%D7%99%20%D7%9E%D7%94%D7%90%D7%AA%D7%A8%20%D7%A9%D7%9C%D7%9A%20%D7%95%D7%90%D7%A9%D7%9E%D7%97%20%D7%9C%D7%A9%D7%9E%D7%95%D7%A2%20%D7%A4%D7%A8%D7%98%D7%99%D7%9D
+
     },
     {
       icon: Mail,
